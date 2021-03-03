624f5a4a71872b6d182a955ec605ec958a6c2b6c
davidmangueira <78989517+davidmangueira@users.noreply.github.com>
2021-03-03 17:15:26 -0300

    Add files via upload

---
+++ Checkador de IP/Checkador de IP/Form1.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace Checkador_de_IP
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+
+        }
+
+    }
+private void button1_Click(object sender, EventArgs e)
+        {
+        List<string> ip = new List<string>();
+
+        // Leitura do arquivo
+
+        using (StreamReader reader = new StreamReader(@"C:\Users\55199\Desktop\2º Semestre\Janaine - Linguagem de Programação\Checkador de IP\Checkador de IP\ip.txt")) 
+        {
+            while(!reader.EndOfStream)
+            {
+                string linha = reader.ReadLine();
+
+                ip.Add(linha);
+            }
+        }
+
+
+    }
+    }
+    }
+}
